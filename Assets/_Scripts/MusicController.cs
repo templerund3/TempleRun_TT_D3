2a9a68cdf085d77f0a5672de40727d6ecb29c29b
KhanhDat <keepyoursmile.512@gmail.com>
2018-05-08 09:08:43 +0700

    them do hoa

---
+++ Assets/_Scripts/MusicController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicController : MonoBehaviour 
+{
+    [SerializeField]
+    private bool isOn;
+
+    public bool IsOn
+    {
+        get { return isOn; }
+        set
+        {
+            isOn = value;
+            UpdateButtons();
+        }
+    }
+
+    public Button buttonMusic;
+    public Sprite isOnSprite;
+    public Sprite isOffSprite;
+
+    public AudioSource audioSource;
+
+    private void Start()
+    {
+        isOn = true;
+    }
+
+    private void UpdateButtons()
+    {
+        buttonMusic.gameObject.GetComponent<Image>().sprite = isOn ? isOnSprite : isOffSprite;
+    }
+
+    public void OnClickButton()
+    {
+        if(isOn)
+        {
+            PlayNewMusic();
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    private IEnumerator PlayNewMusic()
+    {
+        while (audioSource.volume >= 0.1f)
+        {
+            audioSource.volume -= 0.2f;
+            yield return new WaitForSeconds(0.1f);
+        }
+        audioSource.Stop();
+        if (isOn)
+        {
+            audioSource.Play();
+        }
+        audioSource.volume = 1;
+    }
+}
