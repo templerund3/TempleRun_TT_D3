9ae5caddde9755e28cfe2b0b5b41fec424870d8f
LOCNT-TT-D3\nguye <nguyenloc271096@gmail.com>
2018-05-08 08:14:50 +0700

    no message

---
+++ Assets/_Scripts/MonoSingleton.cs
@@ -0,0 +1,23 @@
+﻿//Made by Helios.ĐẠT.Jr
+
+using UnityEngine;
+
+public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
+{
+    public static T instance = null;
+
+    public static T Instance
+    {
+        get
+        {
+            instance = instance ?? (FindObjectOfType(typeof(T)) as T);
+            instance = instance ?? new GameObject(typeof(T).ToString(), typeof(T)).GetComponent<T>();
+            return instance;
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        instance = null;
+    }
+}
