5eebf30e8cd9716d970aff3ad2a2b911123e1b32
KhanhDat <keepyoursmile.512@gmail.com>
2018-05-08 08:35:34 +0700

    sua scene Home

---
+++ Assets/_Scripts/UIManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour 
+{
+    public void btnPlay()
+    {
+
+    }
+
+    public void btnShop()
+    {
+
+    }
+
+    public void btnShare()
+    {
+
+    }
+
+    public void btnLeaderBoard()
+    {
+
+    }
+
+    public void btnMusic()
+    {
+
+    }
+}
